c7c202f3e2b054cf5c0e3e0f6363fe625155be18
julionovoa <julio.novoa@outlook.com>
2022-05-19 13:16:40 -0700

    this is the beginning

---
+++ PrimitiveTypes/src/SomeTypes.java
@@ -0,0 +1,10 @@
+public class SomeTypes {
+    public static void main(String[] args){
+        char myChar = 'J';
+        char myUnicode = '\u00A9';
+        System.out.println(myUnicode);
+
+        boolean isThisTrue = true;
+        boolean isThisFalse = false;
+    }
+}
